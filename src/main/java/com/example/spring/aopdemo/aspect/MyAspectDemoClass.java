993b639f6ae5795f69380e80850dd9a76ec41319
karanara <karanara@mail.uc.edu>
2023-12-03 17:55:20 -0500

    pointcut declarations

--- src/main/java/com/example/spring/aopdemo/aspect/MyAspectDemoClass.java
+++ src/main/java/com/example/spring/aopdemo/aspect/MyAspectDemoClass.java
@@ -2,15 +2,24 @@ package com.example.spring.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyAspectDemoClass {
 
-	@Before("execution(* com.example.spring.aopdemo.dao.*.*(..))")
+	@Pointcut("execution(* com.example.spring.aopdemo.dao.*.*(..))")
+	public void AdviceForPackage() {}
+	
+	
+	@Before("AdviceForPackage()")
 	public void BeforeMethodExecution() {
 		System.out.println("Execution-------------- using before advice  Aspect Oriented Programming");
 		
+	}
+	@Before("AdviceForPackage()")
+	public void PerformAPIDesign() {
+		System.out.println("Performing api deisgn at Before advice");
 	}
 }
