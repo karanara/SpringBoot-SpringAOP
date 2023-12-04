f42f45d1636604e3052392e247e638850ffa7627
karanara <karanara@mail.uc.edu>
2023-12-04 12:40:01 -0500

    Getting method call name and arguments in aspect using JoinPoint

--- src/main/java/com/example/spring/aopdemo/aspect/MyAspectDemoClass.java
+++ src/main/java/com/example/spring/aopdemo/aspect/MyAspectDemoClass.java
@@ -1,21 +1,33 @@
 package com.example.spring.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.example.spring.aopdemo.dao.Account;
 
 @Aspect
 @Component
 @Order(3)
 public class MyAspectDemoClass {
-
-	
 	
 	@Before("com.example.spring.aopdemo.aspect.PointCutExpression.adviceForNoSetterAndGetter()")
-	public void BeforeMethodExecution() {
+	public void BeforeMethodExecution(JoinPoint joinPoint) {
 		System.out.println(getClass()+"     -------------- using before advice  in Aspect Demo Aspect Oriented Programming");
-	
+		MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
+		System.out.println("Method ::  "+methodSignature);
+	    Object[] args=joinPoint.getArgs();
+	    for(Object temp :args) {
+	    	System.out.println("argument ::: "+ temp);
+	    	if(temp instanceof Account) {
+	    		Account account = (Account)temp;
+	    		System.out.println("Account First Name :"+account.getFirstName());
+	    		System.out.println("Account Last Name  :"+account.getLastName());
+	    	}
+	    }
 	}
 }
